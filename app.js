9056bf5e931ba11df01160e3106f45fccac6c4e5
MassMatsu <masa10matsukawa@gmail.com>
2021-05-07 08:48:27 +0900

    refactored and added comments

--- app.js
+++ app.js
@@ -8,17 +8,16 @@ const purchase = document.getElementById('purchase');
 const successMsg = document.getElementById('message');
 const purchaseMsg = document.getElementById('ticket-info');
 const instruction = document.getElementById('instruction')
-console.log(instruction)
 
-let ticketPrice = +movieSelect.value;
-
+// a single movie data
 let data = {
   movieIndex: 0,
   seats: [],
   moviePrice: 10,
 };
 
-let tickets = [];
+// all purchase history
+let transactions = [];
 
 // fetch localstorage data and set to global variable 'data' used as the copy of current localstorage
 const setData = (updates = {}) => {
@@ -47,13 +46,12 @@ const populateUI = () => {
 
   if (data !== null) {
     movieSelect.selectedIndex = data.movieIndex;
-    ticketPrice = data.moviePrice;
   }
 };
 
 //render purchase messages
 const populateMessage = () => {
-  const data = JSON.parse(localStorage.getItem('tickets'));
+  const data = JSON.parse(localStorage.getItem('transactions'));
   
   if (data && data[data.length - 1].seats.length > 0) {
     const { movieIndex, seats, moviePrice } = data[data.length - 1];
@@ -74,60 +72,69 @@ const populateMessage = () => {
   } 
 };
 
-// update total and count, also UI
-const updateSelectedCount = () => {
+// update selected seats data, total and count, also UI
+const updateSeatsAndSummary = () => {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  // get amount of selected seats and use it for count of seats and total price
   const seatsCount = selectedSeats.length;
-
   count.innerText = seatsCount;
-  total.innerText = seatsCount * ticketPrice;
+  total.innerText = seatsCount * data.moviePrice;
 
+  // check selected seats and update data on localstorage
   const seatsIndex = [...selectedSeats].map((seat) => {
     return [...allSeats].indexOf(seat);
   });
   setData({ ...data, seats: seatsIndex });
 };
 
+// reset data on local strage, global variable, and class 'selected' for each seat
 const resetData = () => {
+  // remove localStorage movieData
   localStorage.removeItem('movieData');
+  
+  // set global data to default
   data = {
     movieIndex: 0,
     seats: [],
     moviePrice: 10,
   };
+  
+  // remove selected class off all seats
   allSeats.forEach((seat) => {
     seat.classList.remove('selected');
   });
-  updateSelectedCount();
+
+  // update summary and its UI
+  updateSeatsAndSummary();
 };
 
 // select seats by clicking and add class to node
 container.addEventListener('click', (e) => {
-  if (
+  // add 'selected' class on the clicked seat excluding a seat with 'seat occupied' class 
+  if ( 
     e.target.classList.contains('seat') &&
     !e.target.classList.contains('occupied')
   ) {
     e.target.classList.toggle('selected');
-    updateSelectedCount();
+    updateSeatsAndSummary();
   }
 });
 
 // get value of selected movie
 movieSelect.addEventListener('change', (e) => {
-  ticketPrice = +e.target.value;
 
   setData({
     ...data,
     movieIndex: e.target.selectedIndex,
     moviePrice: +e.target.value,
   });
-  updateSelectedCount();
+  updateSeatsAndSummary();
 });
 
 // purchase event
 purchase.addEventListener('click', () => {
-  tickets.push(data);
-  localStorage.setItem('tickets', JSON.stringify(tickets));
+  transactions.push(data);
+  localStorage.setItem('transactions', JSON.stringify(transactions));
 
   resetData();
   setData();
